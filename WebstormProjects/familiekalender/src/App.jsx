e6a574fb7033143101e67f686a1b4bd82a63a53c
ktanis1988 <k.tanis88@gmail.com>
2026-01-28 14:17:47 +0100

    Toevoegen invoervelden en buttons startpagina  Homepage.jsx

--- WebstormProjects/familiekalender/src/App.jsx
+++ WebstormProjects/familiekalender/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import Homepage from "./pages/Homepage.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
