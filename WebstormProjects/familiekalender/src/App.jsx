fb3ebd1615c723219c7dfc851cf8e98bd90914f3
ktanis1988 <k.tanis88@gmail.com>
2026-02-25 11:44:38 +0100

    Toevoeging routes Addactivitypage aan App.jsx

--- WebstormProjects/familiekalender/src/App.jsx
+++ WebstormProjects/familiekalender/src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Registerpage from "./pages/Registerpage";
 import Calendarpage from "./pages/Calendarpage";
+import Addactivitypage from "./pages/Addactivitypage";
 import "./App.css";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/" element={<Registerpage />} />
-                <Route path="/" element={<Calendarpage />} />
+                <Route path="/homepage" element={<Homepage />} />
+                <Route path="/register" element={<Registerpage />} />
+                <Route path="/calendar" element={<Calendarpage />} />
+                <Route path="/addactivity" element={<Addactivitypage />} />
             </Routes>
         </Router>
     );
