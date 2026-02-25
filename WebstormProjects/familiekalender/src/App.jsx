8442745dc48a54b5c56bbe8eddc063ebbaaea3ef
ktanis1988 <k.tanis88@gmail.com>
2026-02-25 08:57:58 +0100

    Route Calendarpage toegevoegd aan App.jsx

--- WebstormProjects/familiekalender/src/App.jsx
+++ WebstormProjects/familiekalender/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Registerpage from "./pages/Register";
+import Registerpage from "./pages/Registerpage";
+import Calendarpage from "./pages/Calendarpage";
 import "./App.css";
 
 function App() {
@@ -9,10 +10,11 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<Homepage />} />
-                <Route Path="/" element={<Registerpage />} />
+                <Route path="/" element={<Registerpage />} />
+                <Route path="/" element={<Calendarpage />} />
             </Routes>
         </Router>
-    )
+    );
 }
 
 export default App;
