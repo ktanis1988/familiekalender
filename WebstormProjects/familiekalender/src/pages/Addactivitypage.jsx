c842b3824a1e4b9420d87acd8f4ed66dd8f37c51
ktanis1988 <k.tanis88@gmail.com>
2026-03-01 17:25:09 +0100

    navigatie toegevoegd plus wat aanpassingen

--- WebstormProjects/familiekalender/src/pages/Addactivitypage.jsx
+++ WebstormProjects/familiekalender/src/pages/Addactivitypage.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import ".Addactivitypage.css";
+import { useNavigate } from "react-router-dom";
+import "./Addactivitypage.css";
 
 function Addactivitypage() {
+    const navigate = useNavigate();
+
+    function gaTerug() {
+        console.log("Terug knop geklikt");
+        navigate('/calendar');
+    }
+
+    function handleOpslaan(e) {
+        e.preventDefault();
+        console.log("Opslaan knop geklikt");
+        /* later toe te voegen */
+        navigate('/calendar');
+    }
+
+    function handleAnnuleren() {
+        console.log("Annuleren knop geklikt");
+        navigate('/calendar');
+    }
+
     return (
         <div className="addactivity-container">
             <header className="addactivity-header">
-                <button className="back-button"></button>
+                <button className="back-button"onClick={gaTerug}>Vorige</button>
                 <h1>Activiteit toevoegen</h1>
             </header>
 
-            <form className="addactivity-form">
+            <form className="addactivity-form" onSubmit={handleOpslaan}>
                 <div className="form-field">
                     <label>Titel</label>
                     <input type="text" placeholder="Naam activiteit" />
@@ -17,12 +37,12 @@ function Addactivitypage() {
 
                 <div className="form-field">
                     <label>Data</label>
-                    <input type="text" placeholder="Datum activiteit" />
+                    <input type="date" placeholder="Datum activiteit" />
                 </div>
 
                 <div className="form-field">
                     <label>Tijd</label>
-                    <input type="text" placeholder="Tijd activiteit" />
+                    <input type="time" placeholder="Tijd activiteit" />
                 </div>
 
                 <div className="form-field">
@@ -36,7 +56,7 @@ function Addactivitypage() {
                 </div>
 
                 <button type="submit" className="submit-button">Opslaan</button>
-                <button type="button" className="cancel-button">Annuleren</button>
+                <button type="button" className="cancel-button" onClick={handleAnnuleren}>Annuleren</button>
             </form>
         </div>
     );
