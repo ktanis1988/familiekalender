0d61cdb363d973e76c3f49e4b5276e1e7d8dca1b
ktanis1988 <k.tanis88@gmail.com>
2026-03-01 10:51:54 +0100

    navigatie werkend gemaakt op Homepage.jsx

--- WebstormProjects/familiekalender/src/pages/Homepage.jsx
+++ WebstormProjects/familiekalender/src/pages/Homepage.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Homepage.css";
 
 function Homepage() {
+    const navigate = useNavigate();
+
+    function handleInloggen(e) {
+        e.preventDefault();
+        console.log("Inloggen knop geklikt");
+
+        /* Later echte inlogcheck maken */
+        navigate('/Calendar');
+    }
+
+    function gaNaarRegistreren() {
+        console.log("Registreren knop geklikt");
+        navigate('/Register');
+    }
+
+    function wachtwoordVergeten () {
+        console.log("Wachtwoord vergeten geklikt")
+        /* Pagina maken voor wachtwoord reset */
+        alert("Deze functie maak ik later!");
+    }
+
     return (
-        <div>
+        <div className="homepage-container">
             <h1>Welkom bij de Familiekalender!</h1>
             <p>Log in voor je persoonlijke omgeving!</p>
 
-            <form>
+            <form onSubmit={handleInloggen}>
                 <label htmlFor="email">E-mailadres:</label><br />
                 <input type="email" id="email" name="email" placeholder="Voer je e-mailadres in" /><br /><br />
 
@@ -15,8 +37,8 @@ function Homepage() {
                 <input type="password" id="password" name="password" placeholder="Voer je wachtwoord in" /><br /><br />
 
                 <button type="submit">Inloggen</button><br />
-                <button type="button">Registreren</button><br />
-                <button type="button">Wachtwoord vergeten?</button><br />
+                <button type="button" onClick={gaNaarRegistreren}>Registreren</button><br />
+                <button type="button" onClick={wachtwoordVergeten}>Wachtwoord vergeten?</button><br />
             </form>
         </div>
     );
