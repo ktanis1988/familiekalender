0f5cc20b3d17714b30a9021328b8879b42200c4f
ktanis1988 <k.tanis88@gmail.com>
2026-03-01 21:42:26 +0100

    navigatie toegevoegd plus wat kleine aanpassingen

--- WebstormProjects/familiekalender/src/pages/Dayviewpage.jsx
+++ WebstormProjects/familiekalender/src/pages/Dayviewpage.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Dayviewpage.css";
 
 function Dayviewpage() {
+    const navigate = useNavigate();
+
+    function gaTerug() {
+        console.log("Terug naar de kalender");
+        navigate('/calendar');
+    }
+
+    function vorigeDag() {
+        console.log("Vorige dag knop geklikt")
+        /* later toe te voegen */
+    }
+
+    function volgendeDag() {
+        console.log("Volgende dag knop geklikt");
+        /* later toe te voegen */
+    }
+
+    function gaNaarToevoegen() {
+        console.log("Nieuwe activiteit toevoegen");
+        navigate('/addactivity');
+    }
+
+    function handleOpslaan() {
+        console.log("Opslaan knop geklikt");
+        /* later toe te voegen */
+    }
+
     return (
         <div className="dayview-container">
             <header className="dayview-header">
-                <button className="nav-arrow left">Vorige</button>
+                <button className="nav-arrow left" onClick={vorigeDag}>Vorige</button>
                 <h1>Dinsdag 12 Januari 2026</h1>
-                <button className="nav-arrow right">Volgende</button>
+                <button className="nav-arrow right" onClick={volgendeDag}>Volgende</button>
             </header>
 
             <div className="timeslots">
@@ -52,8 +80,8 @@ function Dayviewpage() {
             </div>
             </div>
 
-        <button className="save-button">Opslaan</button>
-        <button className="add-button">+</button>
+        <button className="save-button" onClick={handleOpslaan}>Opslaan</button>
+        <button className="add-button" onClick={gaNaarToevoegen}>+</button>
         </div>
     );
 }
