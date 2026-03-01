af0fe7170459da8b27627d77602ecef0943db187
ktanis1988 <k.tanis88@gmail.com>
2026-03-01 11:41:32 +0100

    navigatie toegevoegd en wat kleine aanpassingen

--- WebstormProjects/familiekalender/src/pages/Registerpage.jsx
+++ WebstormProjects/familiekalender/src/pages/Registerpage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Registerpage.css";
 
 function Registerpage() {
+    const navigate = useNavigate();
+
+    function handleRegisteren(e) {
+        e.preventDefault();
+        console.log("Registreren knop geklikt");
+
+        /* Hier maak ik later de API call en validatie */
+        navigate('/calendar');
+    }
+
+    function gaNaarInloggen(){
+        console.log("Terug naar inloggen");
+        navigate('/homepage');
+    }
+
     return (
         <div className="registerpage-container">
             <h1>Maak een gezinsaccount aan!!</h1>
             <p>Organiseer alles samen!!</p>
 
-            <form className="register-form-container">
+            <form className="register-form-container" onSubmit={handleRegisteren}>
                 <label htmlFor="name">Naam:</label>
                 <input type="text" id="name" name="name" placeholder="Naam:" />
 
@@ -24,7 +40,7 @@ function Registerpage() {
 
                 <p className="login-link">Heb je al een account? Klik hier om in te loggen</p>
 
-                <button type="button" className="button secondary">Inloggen</button>
+                <button type="button" className="button secondary" onClick={gaNaarInloggen}>Inloggen</button>
             </form>
         </div>
     );
