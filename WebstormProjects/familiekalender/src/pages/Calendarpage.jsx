86a6e8f8f04e811838fd574772605aad512c2a34
ktanis1988 <k.tanis88@gmail.com>
2026-03-01 15:50:57 +0100

    navigatie toegevoegd plus wat aanpassingen

--- WebstormProjects/familiekalender/src/pages/Calendarpage.jsx
+++ WebstormProjects/familiekalender/src/pages/Calendarpage.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import ".Calendarpage.css";
+import { useNavigate } from "react-router-dom";
+import "./Calendarpage.css";
 
 function Calendarpage () {
+    const navigate = useNavigate();
+
+    function gaNaarActiviteitToevoegen() {
+        console.log("Plus knop geklikt");
+        navigate('/addactivity');
+    }
+
+    function gaNaarDagweergave() {
+        console.log("Dag geklikt");
+        navigate('/dayview');
+    }
+
+    function vorigeWeek() {
+        console.log("Vorige week knop geklikt");
+        /* later toe te voegen  */
+    }
+
+    function volgendeWeek() {
+        console.log("Volgende week knop geklikt");
+        /* later toe te voegen */
+    }
+
     return (
-        <div className="calender-containter">
+        <div className="calendar-container">
             <header className="calendar-header">
-                <button className="nav-button left"></button>
+                <button className="nav-button left" onClick={vorigeWeek}>Vorige</button>
                 <h1>Gezinskalender</h1>
-                <button className="nav-button right"></button>
+                <button className="nav-button right" onClick={volgendeWeek}>Volgende</button>
             </header>
 
-            <div className="calender-grid">
+            <div className="calendar-grid">
                 <div className="day-header">Maandag</div>
                 <div className="day-header">Dinsdag</div>
                 <div className="day-header">Woensdag</div>
@@ -19,12 +42,12 @@ function Calendarpage () {
                 <div className="day-header">Zaterdag</div>
                 <div className="day-header">Zondag</div>
 
-                {Array.from({ length: 365}).map((_, index) => (
-                        <div key={index} className="calender-cell"></div>
+                {Array.from({ length: 35}).map((_, index) => (
+                        <div key={index} className="calendar-cell" onClick={gaNaarDagweergave}>{/*later toe te voegen*/}</div>
                     ) ) }
             </div>
 
-            <button className="add-button">+</button>
+            <button className="add-button" onClick={gaNaarActiviteitToevoegen}>+</button>
         </div>
     );
 }
